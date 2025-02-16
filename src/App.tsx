a6b3e0cf126b784b6136e658137d6d9735f1a026
akshanshu <akshanshu@dentalkart.com>
2025-02-16 10:34:27 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-red-500 p-4">
+  <h1 className="text-white text-2xl">Welcome to Netflix GPT</h1>
+</div>
+  )
+}
+
+export default App
